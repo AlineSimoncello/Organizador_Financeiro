8dde0fc76e57b84d7ebf99f7f4988b6a57c45876
AlineSimoncello <alineperella@gmail.com>
2024-09-24 14:40:13 -0300

    Primeiro commit

---
+++ src/components/Header/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 150px;
+  text-align: center;
+  background: #1E90FF;
+`
+
+export const Header = styled.h1``;
+
+export const Title = styled.div`
+  padding-top: 30px;
+  color: #fff;
+`;
